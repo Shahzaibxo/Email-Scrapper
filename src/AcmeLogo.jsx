44d40156489a5e3dab2c02c0a33de8619719db30
Shahzaibxo <shahziwork@gmail.com>
2024-03-02 00:39:20 +0500

    changing styles

--- src/AcmeLogo.jsx
+++ src/AcmeLogo.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Logo from './assets/Untitled design (2).mp4'
+import Logo from './assets/Untitled design (2).gif'
 
 export const AcmeLogo = () => (
-  <video src={Logo} autoPlay
+  <img src={Logo} autoPlay
   loop
   muted height="63" width="63" />
 );
